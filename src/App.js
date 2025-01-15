29da78c6a99481352f09d9ebae0ccbb522e491f4
perlakay <perla.kay22@gmail.com>
2025-01-14 19:20:21 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+const Person = (props) => {
+  return(
+    <>
+      <h1>Name: {props.name} </h1>
+      <h2>Last Name: {props.lastName} </h2>
+      <h2>Age: {props.age} </h2>
+    </>
+  )
+}
+
+const App = () => {
+  return (
+    <div className="App">
+      <Person name="John" lastName="Doe" age="25" /> 
+      <Person />  
+      
+    </div>
+  );
+}
+
+export default App;
